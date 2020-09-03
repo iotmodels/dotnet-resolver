a1dde4e57463e5b0a12982d0349fcb6998b79a50
ridomin <ridomin@live.com>
2020-09-02 19:05:36 -0700

    first commit

---
+++ dtdl2-validator/Program.cs
@@ -0,0 +1,47 @@
+﻿using iotmodels_resolver;
+using Microsoft.Azure.DigitalTwins.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace dtdl2_validator
+{ 
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string input = ValidateInput(args);
+            ModelParser parser = new ModelParser();
+            parser.DtmiResolver = PublicRepoResolver.DtmiResolver;
+            try
+            {
+                var parserResult = await parser.ParseAsync(new string[] { File.ReadAllText(input) });
+                foreach (var item in parserResult.Values)
+                {
+                    Console.WriteLine(item.Id);
+                }
+            }
+            catch (ResolutionException rex)
+            {
+                Console.WriteLine(rex.ToString());    
+            }
+            catch (ParsingException pex)
+            {
+                Console.WriteLine(pex.ToString());
+            }
+        }
+
+        static string ValidateInput(string[] args)
+        {
+            string input;
+            if (args.Length < 1)
+            {
+                Console.WriteLine("File to validate?");
+                input = Console.ReadLine();
+            }
+            input = args[0];
+            return input;
+        }
+    }
+}
---
+++ iotmodels-resolver/PublicRepoResolver.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.DigitalTwins.Parser;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace iotmodels_resolver
+{
+    class modelindexitem
+    {
+        public string path { get; set; }
+        public string[] depends { get; set; }
+    }
+
+    public static class PublicRepoResolver
+    {
+        const  string modelRepoUrl = "https://iotmodels.github.io/registry/";
+        static WebClient wc = new WebClient();
+        static IDictionary<string, modelindexitem> index;
+
+        static PublicRepoResolver()
+        {
+            Console.Write("Downloading Index.. ");
+            var modelIndexJson = wc.DownloadString(modelRepoUrl + "model-index.json");
+            index = JsonConvert.DeserializeObject<IDictionary<string, modelindexitem>>(modelIndexJson);
+            Console.WriteLine(".. Loaded !!");
+        }
+
+        static public async Task<IEnumerable<string>> DtmiResolver(IReadOnlyCollection<Dtmi> dtmis)
+        {
+            List<string> resolvedModels = new List<string>();
+            foreach (var dtmi in dtmis)
+            {
+                if (index.ContainsKey(dtmi.AbsoluteUri))
+                {
+                    string url = modelRepoUrl + index[dtmi.AbsoluteUri].path;
+                    Console.WriteLine("Downloading definition");
+                    resolvedModels.Add(wc.DownloadString(url));
+                    Console.WriteLine("OK " + url);
+                }
+            }
+            return await Task.FromResult(resolvedModels);
+        }
+    }
+}
