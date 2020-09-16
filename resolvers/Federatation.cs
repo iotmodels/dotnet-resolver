31af70bf6ff84d79f41c47d00eb3f4400dad734f
ridomin <ridomin@live.com>
2020-09-16 01:21:47 -0700

    change config

--- resolvers/Federatation.cs
+++ resolvers/Federatation.cs
@@ -39,10 +39,8 @@ namespace IoTModels.Resolvers
                     foreach (string repo in repoList)
                     {
                         logger.LogInformation($"Resolving {dtmi.AbsoluteUri} in repo {repo}.");
-                        var path = DtmiConvention.Dtmi2Path(dtmi.AbsoluteUri);
+                        string url = repo + DtmiConvention.Dtmi2Path(dtmi.AbsoluteUri);
 
-                        string url = repo + path;
-                        logger.LogTrace("Request: " + url);
                         if (await http.Head(url, logger))
                         {
                             resolvedModels.Add(await http.Get(url, logger));
@@ -50,6 +48,12 @@ namespace IoTModels.Resolvers
                             logger.LogTrace("Found:" + url);
                             break;
                         }
+                        else
+                        {
+                            logger.LogTrace("Not Found:" + url);
+                        }
+                            
+
                     }
                 }
             }
